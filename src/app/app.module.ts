b2163ee60eff3ee9728eb9015ec97f45c2660eba
Gabriel <gabrielmelconian@gmail.com>
2021-07-21 00:44:56 -0300

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmpleadosComponent } from './empleados/empleados.components';
+import { EmpleadoComponent } from './empleado/empleado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, EmpleadosComponent, EmpleadoComponent
+
   ],
   imports: [
     BrowserModule
